e75a662d9153356634eba9678c2b57c21880544f
Eyalyehieli <58137983+Eyalyehieli@users.noreply.github.com>
2022-05-28 15:56:01 +0300

    Delete jsonHandle.h

--- NFQUEUE/nfqFilterPkt/jsonHandle.h
+++
@@ -1,8 +0,0 @@
-#ifndef JSONHANDLE_H_INCLUDED
-#define JSONHANDLE_H_INCLUDED
-#include <json-c/json.h>
-
-void append_json_object_to_file(const char* fileName,char* obj);
-
-
-#endif // JSONHANDLE_H_INCLUDED
